86b6f0bd03fe17ab07a76f303fb8a23e69121820
sayed younis <elsaydseif17@gmail.com>
2024-07-21 07:05:09 +0300

    finish login auth.

--- lib/Views/login_screen.dart
+++ lib/Views/login_screen.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:whats_app/Views/chat_screen.dart';
 import 'package:whats_app/Widgets/custom_button.dart';
 import 'package:whats_app/Widgets/custom_text.dart';
 import 'package:whats_app/Widgets/custom_text_form_feild.dart';
+import 'package:whats_app/Widgets/loading_indicator.dart';
+import 'package:whats_app/Widgets/snack_bar.dart';
 import 'package:whats_app/constants.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
   static String id = 'loginPage';
+  late String email, password;
 
   @override
   Widget build(BuildContext context) {
@@ -32,26 +36,41 @@ class LoginScreen extends StatelessWidget {
                         color: kPrimeryColor),
                   ],
                 ),
-                 CustomTextFormField(
-                  onChange: (data) {},
-                  labelText: 'Name',
-                ),
-                 CustomTextFormField(
-                  onChange: (data) {},
+                CustomTextFormField(
+                  onChange: (data) {
+                    email = data;
+                  },
                   labelText: 'Email',
                 ),
-                 CustomTextFormField(
-                  onChange: (data) {},
+                CustomTextFormField(
+                  onChange: (data) {
+                    password = data;
+                  },
                   labelText: 'Password',
                   obsecureText: true,
                 ),
                 CustomButton(
+                  width: double.infinity,
                   buttonText: 'Login',
                   buttonColor: kPrimeryColor,
-                  onPressed: () {
-                    Navigator.pushNamed(context, ChatScreen.id);
+                  onPressed: () async {
+                    try {
+                      await UserLogin();
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'user-not-found') {
+                        if (context.mounted) {
+                          showSnackBar(context, 'user-not-found');
+                        }
+                      } else if (e.code == 'wrong-password') {
+                        if (context.mounted) {
+                          showSnackBar(context, 'wrong - password');
+                        }
+                      }
+                    } catch (e) {
+                      if (context.mounted) showSnackBar(context, 'error');
+                    }
                   },
-                  width: double.infinity,
                 ),
               ],
             ),
@@ -59,5 +78,10 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> UserLogin() async {
+    UserCredential user = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
   }
 }
