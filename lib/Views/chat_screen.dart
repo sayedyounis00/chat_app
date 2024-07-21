669be816159fa7f0cca0e5116184849965ff23bc
sayed younis <elsaydseif17@gmail.com>
2024-07-21 08:16:52 +0300

    adding firestore database

--- lib/Views/chat_screen.dart
+++ lib/Views/chat_screen.dart
@@ -1,33 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:whats_app/Models/message_model.dart';
 import 'package:whats_app/Widgets/sending_form.dart';
 import 'package:whats_app/Widgets/user_bar.dart';
 import 'package:whats_app/Widgets/user_chat_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+  ChatScreen({super.key});
   static String id = 'ChatScreen';
+  final TextEditingController controller = TextEditingController();
+  final String dateCreated=DateFormat('h:mm:ss a').format(DateTime.now());
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors
-            .grey, // Image.asset('assets/Images/scaffoldIamge.jpg').color,
-        body: Column(
-          children: [
-            const UserBar(),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 20,
-                itemBuilder: (context, index) {
-                  return const UserChatBubble();
-                },
+    CollectionReference messages =
+        FirebaseFirestore.instance.collection('messages');
+
+    return StreamBuilder<QuerySnapshot>(
+        stream:  messages.orderBy('createdAt',descending: true).snapshots(),
+        builder: (context, snapshots) {
+          if (snapshots.hasData) {
+            List<MessageModel> messageList = [
+            ];
+            for (int i = 0; i <snapshots.data!.docs.length; i++) {
+              messageList.add(MessageModel.fromjson(snapshots.data!.docs[i]));
+            }
+            return SafeArea(
+              child: Scaffold(
+                backgroundColor: Colors
+                    .grey, // Image.asset('assets/Images/scaffoldIamge.jpg').color,
+                body: Column(
+                  children: [
+                    const UserBar(),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: messageList.length,
+                        itemBuilder: (context, index) {
+                          return UserChatBubble(
+                            userMessage: messageList[index],
+                          );
+                        },
+                      ),
+                    ),
+                    SendingForm(
+                      onSubmitted: (message) {
+                        messages.add({
+                          'message': message,
+                          'createdAt':dateCreated,
+                        }).then(
+                          (value) => message,
+                        );
+                        controller.clear();
+                      },
+                      controller: controller,
+                    )
+                  ],
+                ),
               ),
-            ),
-            const SendingForm(),
-          ],
-        ),
-      ),
-    );
+            );
+          } else {
+            return const Text('Error');
+          
+          }
+        });
   }
 }
