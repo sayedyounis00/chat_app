114e0581dabb67cd5d148998ed872e85baca225f
sayed younis <elsaydseif17@gmail.com>
2024-07-21 08:16:36 +0300

    pass data from firebase to chat bubble

--- lib/Widgets/user_chat_bubble.dart
+++ lib/Widgets/user_chat_bubble.dart
@@ -1,17 +1,18 @@
-
 import 'package:flutter/material.dart';
+import 'package:whats_app/Models/message_model.dart';
 import 'package:whats_app/Widgets/custom_text.dart';
 import 'package:whats_app/constants.dart';
 
 class UserChatBubble extends StatelessWidget {
   const UserChatBubble({
-    super.key,
+    super.key, required this.userMessage,
   });
+  final MessageModel userMessage;
 
   @override
   Widget build(BuildContext context) {
     return Align(
-        alignment: Alignment.centerLeft,
+      alignment: Alignment.centerLeft,
       child: Container(
         padding: const EdgeInsetsDirectional.all(10),
         margin: const EdgeInsetsDirectional.all(10),
@@ -23,8 +24,8 @@ class UserChatBubble extends StatelessWidget {
             topRight: Radius.circular(10),
           ),
         ),
-        child: const CustomText(
-          text: 'hello Iam a new user',
+        child:  CustomText(
+          text: userMessage.message,
           color: Colors.white,
         ),
       ),
